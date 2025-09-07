f50936023ddc77d267e70f6178551e50dfc54832
sl-he <61023562+sl-he@users.noreply.github.com>
2025-09-07 14:47:09 +0300

    Update anawbpps.js
    Remove old constant.

--- anawbpps.js
+++ anawbpps.js
@@ -24,12 +24,13 @@
 #include "modules/lights_index.jsh"
 #include "modules/calibration_match.jsh"
 #include "modules/calibration_run.jsh"
+#include "modules/preprocessing_progress.jsh"
 
 // ============================================================================
 // Hardcoded defaults (edit these to your liking)
 // ============================================================================
 var HARDCODED_DEFAULTS = {
-    lights:  "D:/!!!WORK/ASTROFOTO/!!!WORK_LIGHTS", // sample
+    lights:  "D:/!!!WORK/ASTROFOTO/!!!WORK_LIGHTS/Sivan 2", // sample
     masters: "D:/!!!WORK/ASTROFOTO/!!!!!MASTERS",   // sample
     work1:   "D:/!!!WORK/ASTROFOTO/",              // base root (script will append !!!WORK_LIGHTS if needed)
     work2:   "W:/!!!WORK/ASTROFOTO/",              // base root (optional)
@@ -444,11 +445,8 @@ function ANAWBPPSDialog(){
         if (self.cbCal && self.cbCal.checked){
             Console.noteln("[run] Calibration checkbox is ON — running ImageCalibration…");
 
-// Explicit path from constants
-            var xpsmCandidates = [ _norm(CONST_IMAGECAL_XPSM) ];
-
             try{
-                CAL_runCalibration(PLAN, wf /* third arg ignored */);
+                CAL_runCalibration_UI(PLAN, wf);
             } catch(e){
                 Console.criticalln("[run] Calibration failed: " + e);
                 showDialogBox("ANAWBPPS — Calibration Error", String(e));
